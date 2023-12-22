ce54985a39b161d04a9a9497701d52987f7ea6b3
matvei1223 <129781652+matvei1223@users.noreply.github.com>
2023-12-22 22:32:06 +0300

    фикс по скнинам
    фикс по скнинам

--- js/script.js
+++ js/script.js
@@ -66,3 +66,88 @@ document.addEventListener('keydown', (event)=>{
         document.body.style.overflow = '';
     }
 })
+
+
+//slider
+const slides = document.querySelectorAll('.offer__slide'),
+prev = document.querySelector('.offer__slider-prev'),
+next = document.querySelector('.offer__slider-next'),
+total = document.querySelector('#total'),
+current = document.querySelector('#current');
+
+let slideIndex = 1;
+
+function showSlides(n){
+    if(n>slides.length){
+        slideIndex = 1;
+    }
+    if(n<1){
+        slideIndex = slides.length;
+    }
+    if(slides.length <10){
+        total.textContent = `0${slides.length}`;
+    }else{
+        total.textContent = slides.length;
+    }
+    slides.forEach(item=> item.style.display="none");
+    slides[slideIndex-1].style.display ="block";
+
+    if(slides.length<10){
+        current.textContent = `0${slideIndex}`;
+    }else{
+        current.textContent = slideIndex;
+    }
+}
+showSlides(slideIndex);
+
+function plusSlide(n){
+    showSlides(slideIndex = slideIndex+n);
+}
+
+next.addEventListener('click',()=>{
+    plusSlide(1);
+})
+prev.addEventListener('click',()=>{
+    plusSlide(-1);
+})
+
+
+//timer
+const timeEnd = '2024-02-19'
+function getTimeRemaining(endtime){
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t/(1000*60*60*24)),
+        hours = Math.floor((t/(1000*60*60)%24)),
+        minuties = Math.floor(t/((1000*60)%60)),
+        seconds = Math.floor((t/(1000)%60));
+    return{
+        'total':t,
+        'days':days,
+        'hours':hours,
+        'minuties':minuties,
+        'seconds':seconds
+    }
+}
+function setClock(selector, endtime){
+    const timer = document.querySelector(selector),
+        days = document.querySelector('#days'),
+        hours = document.querySelector('#hours'),
+        minuties = document.querySelector('#minuties'),
+        seconds = document.querySelector('#seconds');
+        timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock(){
+        const t = getTimeRemaining(endtime);
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minuties.innerHTML = t.minuties;
+        seconds.innerHTML = t.seconds;
+        if(t.total<=0){
+            days.innerHTML = '00';
+            hours.innerHTML = '00';
+            minuties.innerHTML = '00';
+            seconds.innerHTML = '00';
+        }
+    }
+}
+setClock('.timer', timeEnd);
