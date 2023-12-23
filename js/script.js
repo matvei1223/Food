c4294588268b839069dacd35fece99c069fa8db2
matvei1223 <129781652+matvei1223@users.noreply.github.com>
2023-12-23 14:46:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -111,3 +111,44 @@ prev.addEventListener('click',()=>{
     plusSlide(-1);
 })
 
+// timer
+const timeEnd = '2024-02-19';
+
+function getTimeRemaining(endtime){
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t/(1000*60*60*24)),
+        hours = Math.floor((t/(1000*60*60))%24),
+        minutes = Math.floor((t/(1000*60))%60),
+        seconds = Math.floor((t/(1000))%60);
+    return{
+        'total':t,
+        'days':days,
+        'hours':hours,
+        'minutes':minutes,
+        'seconds':seconds
+    }
+}
+function setClock(selector,endtime){
+    const timer = document.querySelector(selector),
+         days = document.querySelector('#days'),
+         hours = document.querySelector('#hours'),
+         minutes = document.querySelector('#minutes'),
+         seconds = document.querySelector('#seconds');
+         timeInterval = setInterval(updateClock,1000);
+    updateClock();
+    function updateClock(){
+        const t = getTimeRemaining(endtime);
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+        if(t.total<=0){
+            clearInterval(timeInterval);
+            days.innerHTML = "00";
+            hours.innerHTML = "00";
+            minutes.innerHTML = "00";
+            seconds.innerHTML = "00";
+        }
+    }
+}
+setClock('.timer',timeEnd);
